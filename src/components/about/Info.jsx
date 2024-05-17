a3461bfc26dcaf32be81ff7c549a5fd2c98f24dc
Sudhanva <sudhanvahabib@gmail.com>
2024-05-17 17:07:21 +0530

    modifications

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -2,16 +2,18 @@ import React, { useEffect, useState } from 'react'
 
 const Info = () => {
 
+  const [experienceYears, setExperienceYears] = useState(0);
   const [experienceMonths, setExperienceMonths] = useState(0);
 
   useEffect(() => {
-    const calculateExperienceMonths = () => {
+    const calculateExperienceYears = () => {
       const joiningDate = new Date('2022-01-06');
       const currentDate = new Date();
       const diffMonths = (currentDate.getFullYear() - joiningDate.getFullYear()) * 12 + (currentDate.getMonth() - joiningDate.getMonth());
-      setExperienceMonths(diffMonths);
+      setExperienceYears((diffMonths/12).toFixed(0));
+      setExperienceMonths(diffMonths%12)
     };
-    calculateExperienceMonths();
+    calculateExperienceYears();
   }, []);
 
   return (
@@ -19,7 +21,7 @@ const Info = () => {
         <div className="about__box">
             <i className='bx bx-award about__icon'></i>
             <h3 className="about__title">Experience</h3>
-            <span className="about__subtitle">{experienceMonths} Months Working</span>
+            <span className="about__subtitle">{experienceYears} years {experienceMonths>0?`${experienceMonths} ${experienceMonths>1?"months":"month"}`:" "} Working</span>
         </div>
         <div className="about__box">
             <i className='bx bx-briefcase-alt about__icon'></i>
